a7b15613b9e28a927274a86274c4d1934a95c5c1
Bálint Biros <bbalint2000@gmail.com>
2021-03-18 19:54:22 +0100

    Field

--- src/Field.java
+++ src/Field.java
@@ -1,15 +1,60 @@
 import java.util.ArrayList;
 
-public class Field implements Steppable{
+/**
+ * The type Field.
+ */
+public class Field implements Steppable {
     private ArrayList<Asteroid> asteroids;
     private ArrayList<Settler> settlers;
 
     @Override
-    public void Step() {}
+    public void Step() {
+        SetNearSun();
+        SetSunStorm();
+    }
 
-    public void SetSunStorm(){}
-    public void SetNearSun(){}
-    public void RemoveSettler(Settler s){}
-    public void RemoveAsteroid(Asteroid a){}
-    
+
+    /**
+     * Set sun storm.
+     *
+     * Set sunstorm for all asteroids
+     */
+    public void SetSunStorm() {
+        //valamennyi tickenként
+        for (Asteroid a : asteroids) {
+            a.Sunstorm();
+        }
+    }
+
+    /**
+     * Set near sun.
+     *
+     * Set NearSun for specified asteroids and check if any asteroid is triggered
+     */
+    public void SetNearSun() {
+        //valami alapján átállitja az asteroidáknál a nearsunt
+        for (Asteroid a : asteroids) {
+            a.CheckTrigger();
+        }
+
+    }
+
+    /**
+     * Remove settler.
+     *
+     * @param s the settler to remove
+     */
+    public void RemoveSettler(Settler s) {
+        settlers.remove(s);
+    }
+
+    /**
+     * Remove asteroid.
+     *
+     * @param a the asteroid to remove
+     */
+    public void RemoveAsteroid(Asteroid a) {
+        asteroids.remove(a);
+    }
+
 }
