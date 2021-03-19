ccfd1a89fb64b4ea4251e3fcf4901a1435ec8695
borospc <borosgergo@yahoo.com>
2021-03-19 23:27:53 +0100

    Materials done

--- src/Material.java
+++ src/Material.java
@@ -1,4 +1,15 @@
+/**
+ * Abstract class Material.
+ */
 public abstract class Material {
 
+    /**
+     * The Asteroid that contains the material inside it.
+     */
+    Asteroid asteroid;
+
+    /**
+     * Trigger of special materials in case of special events.
+     */
     public abstract void Trigger();
 }
--- src/Uranium.java
+++ src/Uranium.java
@@ -1,7 +1,15 @@
+/**
+ * Uranium, the radioactive type of Material.
+ */
 public class Uranium extends Material{
 
+    /**
+     * If the material is triggered the asteroid explodes,
+     * that kills all settlers and blows away all robots which are on the asteroid.
+     * The trigger happens, if the asteroid is near the sun and the core is exposed.
+     */
     @Override
     public void Trigger() {
-
+        asteroid.Explode();
     }
 }
