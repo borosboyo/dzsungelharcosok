ab505c404f1819ecef9f75eb0afe617140d4a28a
IBarnabas <barnabas.ivancsics@gmail.com>
2021-03-19 14:12:58 +0100

    Game update: Add field param.

--- src/Game.java
+++ src/Game.java
@@ -2,7 +2,7 @@ public class Game {
 
     //Singleton
     public static Game game = new Game();
-
+    Field field = new Field();
     public void EndGame(){}
     public void StartGame(){ }
     public void WinGame() {}
