3bc789ce94fc8a1387122abc0348325fd1fc742f
IBarnabas <barnabas.ivancsics@gmail.com>
2021-03-19 18:47:21 +0100

    Game update: Add RandomMaterial meth

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Game {
 
     //Singleton
@@ -11,5 +13,21 @@ public class Game {
         System.out.println("Hello world");
     }
 
+
+     Material RandomMaterial(){
+        Random rand = new Random();
+        Material mat = null;
+
+        int r_num = rand.nextInt(5);
+        switch (r_num){
+            case 0 -> mat = new Uranium();
+            case 1 -> mat = new Ice();
+            case 2 -> mat = new Coal();
+            case 3 -> mat = new Iron();
+            case 4 -> mat = null;
+        }
+
+        return mat;
+    }
 
 }
