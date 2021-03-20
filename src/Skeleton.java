a9b86033cf1ef1b309a8849af0740af2678f5e2d
Dani <hathazi.daniel@gmail.com>
2021-03-20 11:58:17 +0100

    Skeleton update, még messze a vége

--- src/Skeleton.java
+++ src/Skeleton.java
@@ -1,18 +1,27 @@
 import java.util.Scanner;
 
 public class Skeleton {
-
+    private Scanner input;
+    Skeleton(){
+        input = new Scanner(System.in);
+    }
 
     public static void main(String args[]){
         Skeleton s = new Skeleton();
-        s.TestList();
-        s.chooseTest();
+        int exit = 1;
+         do {
+            s.TestList();
+            s.chooseTest();
+            exit = s.EndTest();
+        }while (exit != 0);
+
     }
 
     /**
      * A tesztek listája
      */
     public void TestList(){
+        System.out.print("\n***************************New Test***************************");
         System.out.print(
                 "Please select a test!\n" +
                         "Press 1 to test Settler Moves\n" +
@@ -41,12 +50,18 @@ public class Skeleton {
         );
     }
 
+    public int EndTest(){
+        System.out.println("**************************Test Ended**************************");
+        System.out.println("Press 0 to exit, press another number to choose another test!");
+        int exit = input.nextInt();
+        return exit;
+    }
+
     /**
      * A felhasználótól beolvas egy számot
      * @return a felhasználó által megadott szám
      */
     public int  inputNumber(){
-        Scanner input = new Scanner(System.in);
         int number = input.nextInt();
         while(number > 23 || number < 1){
             System.out.println("The number was incorrect. Please enter a number between 1-23!");
@@ -61,50 +76,73 @@ public class Skeleton {
         switch (number){
             case 1:
                 Settler_Moves_test();
+                break;
             case 2:
                 Settler_wins_game_with_move_test();
+                break;
             case 3:
-
+                Settler_tries_to_move_on_not_neighbour();
+                break;
             case 4:
-
+                Robot_Moves();
+                break;
             case 5:
-
+                Settler_place_material();
+                break;
             case 6:
-
+                Settler_tries_to_place_material_on_full_asteroid();
+                break;
             case 7:
-
+                Settler_place_teleport();
+                break;
             case 8:
-
+                Settler_makes_teleport();
+                break;
             case 9:
-
+                Settler_uses_teleport();
+                break;
             case 10:
-
+                Settler_tries_to_use_unpaired_teleport();
+                break;
             case 11:
-
+                Robot_uses_teleport();
+                break;
             case 12:
-
+                Robot_tries_to_use_unpaired_teleport();
+                break;
             case 13:
-
+                Sunstorm_on_settler();
+                break;
             case 14:
-
+                Sunstorm_on_robot();
+                break;
             case 15:
-
+                Settler_builds_robot();
+                break;
             case 16:
-
+                Settler_mines();
+                break;
             case 17:
-
+                Settler_drills_non_trigger();
+                break;
             case 18:
-
+                Settler_drills_radioactive();
+                break;
             case 19:
-
+                Settler_drills_ice();
+                break;
             case 20:
-
+                All_settlers_die();
+                break;
             case 21:
-
+                Robot_drills_non_trigger();
+                break;
             case 22:
-
+                Robot_drills_radioactive();
+                break;
             case 23:
-
+                Robot_drills_ice();
+                break;
         }
     }
 
@@ -119,6 +157,7 @@ public class Skeleton {
                 System.out.print("|\t");
             System.out.print(functions[i]+ "\n");
         }
+        System.out.print("\n");
     }
 
     /**
@@ -126,9 +165,6 @@ public class Skeleton {
      */
     public void Settler_Moves_test(){
 
-        /*A meghívandó függvények neveit ki szeretném írni egy egy String[] tömbbe,
-        de ez nem valami szép megoldás..
-         */
         String func[] = {
                 "Settler.Move(Asteroid)",
                 "Asteroid.CheckNeighbour()",
@@ -150,7 +186,68 @@ public class Skeleton {
         };
         WriteTest(func);
     }
-    public void Settler_tries_to_move_on_not_neighbour(){}
-    public void Robot_Moves(){}
+
+    public void Settler_tries_to_move_on_not_neighbour(){
+        String func[] = {
+                "Settler.Move(Asteroid)",
+                "Asteroid.CheckNeighbour()",
+        };
+        WriteTest(func);
+    }
+
+    public void Robot_Moves(){
+        String func[] = {
+                "Robot.Step()",
+                "Asteroid.CheckNeighbour()",
+                "Robot.Move(Asteroid)",
+                "Asteroid.Accept(Robot)",
+                "Asteroid.Remove(Robot)"
+        };
+        WriteTest(func);
+    }
+
+    public void Settler_place_material(){
+        System.out.println("Current crustthickness? (Write a number between 0-10");
+        int crust = input.nextInt();
+        while(crust > 10 || crust < 0){
+            System.out.println("The number was incorrect. Please enter a number between 0-10!)");
+            crust = input.nextInt();
+        }
+        String func[];
+        if(crust == 0)
+        func = new String[]{
+                "Settler.PlaceMaterial()",
+                "Asteroid.CheckNeighbour()",
+                "Robot.Move(Asteroid)",
+                "Asteroid.Accept(Robot)",
+                "Asteroid.Remove(Robot)"
+        };
+        else
+            func = new String[]{
+                    "Settler.PlaceMaterial()"};
+        WriteTest(func);
+    }
+
+    public void Settler_tries_to_place_material_on_full_asteroid(){}
+    public void Settler_place_teleport(){}
+    public void Settler_makes_teleport(){}
+    public void Settler_uses_teleport(){}
+    public void Settler_tries_to_use_unpaired_teleport(){}
+    public void Robot_uses_teleport(){}
+    public void Robot_tries_to_use_unpaired_teleport(){}
+    public void Sunstorm_on_settler(){}
+    public void Sunstorm_on_robot(){}
+    public void Settler_builds_robot(){}
+    public void Settler_mines(){}
+    public void Settler_drills_non_trigger(){}
+    public void Settler_drills_radioactive(){}
+    public void Settler_drills_ice(){}
+    public void All_settlers_die(){}
+    public void Robot_drills_non_trigger(){}
+    public void Robot_drills_radioactive(){}
+    public void Robot_drills_ice(){}
+
+
+
 
 }
