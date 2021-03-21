f317a00d34b7924acadb419273327eea8e364204
borospc <borosgergo@yahoo.com>
2021-03-21 22:03:38 +0100

    Asteroid comments

--- src/Asteroid.java
+++ src/Asteroid.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * The type Asteroid.
+ */
 public class Asteroid {
     private long crustThickness;
     private  boolean nearSun;
@@ -9,36 +12,70 @@ public class Asteroid {
     private ArrayList<Teleport> teleports = new ArrayList();
     private ArrayList<Entity> entities = new ArrayList();
 
+    /**
+     * Initializes a new Asteroid.
+     *
+     * @param crustThickness the crust thickness
+     * @param nearSun        the near sun
+     * @param material       the material
+     */
     public Asteroid(long crustThickness, boolean nearSun, Material material) {
         this.crustThickness = crustThickness;
         this.nearSun = nearSun;
         this.material = material;
     }
 
+    /**
+     * Accept.
+     *
+     * @param e the e
+     */
     public void Accept(Entity e){
         this.entities.add(e);
     }
 
+    /**
+     * Add material.
+     *
+     * @param m the m
+     */
     public void AddMaterial(Material m){
         this.material = m;
     }
 
+    /**
+     * Build teleport.
+     *
+     * @param t the t
+     */
     public void BuildTeleport(Teleport t){
         teleports.add(t);
     }
 
+    /**
+     * Check base.
+     */
     public void CheckBase(){
         if(Game.getInstance().field.CheckReqMat(this)){
             Game.getInstance().WinGame();
         }
     }
 
+    /**
+     * Check trigger.
+     */
     public void CheckTrigger(){
         if(this.nearSun && this.crustThickness == 0){
             material.Trigger();
         }
     }
 
+    /**
+     * Check neighbour boolean.
+     *
+     * @param a the a
+     * @return the boolean
+     */
     public boolean CheckNeighbour(Asteroid a){
         for (Asteroid neighbour : neighbours) {
             if (a == neighbour)
@@ -47,11 +84,19 @@ public class Asteroid {
         return false;
     }
 
+    /**
+     * Drilled by.
+     */
     public void DrilledBy(){
         this.crustThickness--;
         CheckTrigger();
     }
 
+    /**
+     * Mined by material.
+     *
+     * @return the material
+     */
     public Material MinedBy(){
         Material ret_material = material;
         if (ret_material != null && crustThickness == 0){
@@ -62,6 +107,9 @@ public class Asteroid {
         return null;
     }
 
+    /**
+     * Sunstorm.
+     */
     public void Sunstorm(){
         if(crustThickness == 0 && material != null){
             for (Entity entity : entities) {
@@ -70,6 +118,9 @@ public class Asteroid {
         }
     }
 
+    /**
+     * Explode.
+     */
     public void Explode(){
         Game.getInstance().field.RemoveAsteroid(this);
 
@@ -87,18 +138,36 @@ public class Asteroid {
 
     }
 
+    /**
+     * Remove entity.
+     *
+     * @param e the e
+     */
     public void RemoveEntity(Entity e){
         entities.remove(e);
     }
 
+    /**
+     * Remove material.
+     */
     public void RemoveMaterial(){
         material = null;
     }
 
+    /**
+     * Remove neighbour.
+     *
+     * @param a the a
+     */
     public void RemoveNeighbour(Asteroid a){
         neighbours.remove(a);
     }
 
+    /**
+     * Get random neighbour asteroid.
+     *
+     * @return the asteroid
+     */
     public Asteroid GetRandomNeighbour() {
         if (neighbours.size() < 1) {
             return null;
@@ -110,6 +179,11 @@ public class Asteroid {
         return  neighbours.get(r_index);
     }
 
+    /**
+     * Get random teleport teleport.
+     *
+     * @return the teleport
+     */
     public Teleport GetRandomTeleport(){
         if (teleports.size() < 1) {
             return null;
@@ -121,55 +195,119 @@ public class Asteroid {
     }
 
 
-
+    /**
+     * Gets neigbours.
+     *
+     * @return the neigbours
+     */
     public ArrayList<Asteroid> getNeigbours() {
         return neighbours;
     }
 
+    /**
+     * Sets neigbours.
+     *
+     * @param neigbours the neigbours
+     */
     public void setNeigbours(ArrayList<Asteroid> neigbours) {
         this.neighbours = neigbours;
     }
 
+    /**
+     * Gets teleports.
+     *
+     * @return the teleports
+     */
     public ArrayList<Teleport> getTeleports() {
         return teleports;
     }
 
+    /**
+     * Sets teleports.
+     *
+     * @param teleports the teleports
+     */
     public void setTeleports(ArrayList<Teleport> teleports) {
         this.teleports = teleports;
     }
 
+    /**
+     * Gets entities.
+     *
+     * @return the entities
+     */
     public ArrayList<Entity> getEntities() {
         return entities;
     }
 
+    /**
+     * Sets entities.
+     *
+     * @param entities the entities
+     */
     public void setEntities(ArrayList<Entity> entities) {
         this.entities = entities;
     }
 
+    /**
+     * Gets material.
+     *
+     * @return the material
+     */
     public Material getMaterial() {
         return material;
     }
 
+    /**
+     * Sets material.
+     *
+     * @param material the material
+     */
     public void setMaterial(Material material) {
         this.material = material;
     }
 
+    /**
+     * Gets crust thickness.
+     *
+     * @return the crust thickness
+     */
     public long getCrustThickness() {
         return crustThickness;
     }
 
+    /**
+     * Sets crust thickness.
+     *
+     * @param crustThickness the crust thickness
+     */
     public void setCrustThickness(long crustThickness) {
         this.crustThickness = crustThickness;
     }
 
+    /**
+     * Is near sun boolean.
+     *
+     * @return the boolean
+     */
     public boolean isNearSun() {
         return nearSun;
     }
 
+    /**
+     * Sets near sun.
+     *
+     * @param nearSun the near sun
+     */
     public void setNearSun(boolean nearSun) {
         this.nearSun = nearSun;
     }
 
+    /**
+     * Add neighbour.
+     *
+     * @param a the a
+     */
     public void AddNeighbour(Asteroid a) {
         neighbours.add(a);
     }
