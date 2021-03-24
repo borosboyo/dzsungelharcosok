fb83cc3d695d95177b5cf8b1e3dc819957f88675
Bálint Biros <78959832+bbalint2000@users.noreply.github.com>
2021-03-24 17:37:52 +0100

    Update Ufo.java
    Update:
    Ufo

--- src/Ufo.java
+++ src/Ufo.java
@@ -1,13 +1,15 @@
 import java.util.Random;
 
 public class Ufo extends Entity implements Steppable{
+      private ArrayList<Material> inventory;
+
     @Override
     public void Step() {
         Random rand = new Random();
         long crust = getAsteroid().getCrustThickness();
         Teleport teleport = getAsteroid().GetRandomTeleport();
 
-        if ((crust == 0) && (getAsteroid().getMaterial() != null) ) {  //Ha van szomszédja az aszteroidának és 30% akkor teleportál
+        if ((crust == 0) && (getAsteroid().getMaterial() != null) ) {  //Ha tud mine-ol különben mozog 
          mine();
         } else {
             if ((teleport.getAsteroids().size() == 2) && (rand.nextInt(100) < 30)) {
