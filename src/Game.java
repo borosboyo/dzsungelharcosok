1fa839370b2750feecbcd669b822e78d7c53515d
IBarnabas <barnabas.ivancsics@gmail.com>
2021-04-18 02:14:07 +0200

    Game update: Parancsok kezelése a játékhoz

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Scanner;
 
 /**
     * The Game singleton class that manages ending and starting a game.
@@ -50,7 +51,7 @@ public class Game implements Serializable {
     /**
      * The Field that contains all the asteroids and settlers.
      */
-    Field field = new Field(); //TODO: atadni hany settler es max keregvastagsag
+    Field field = new Field(5,3); //TODO: atadni hany settler es max keregvastagsag
 
 
     /**
@@ -96,4 +97,122 @@ public class Game implements Serializable {
         output.writeObject(game);
     }
 
+    //TODO::ezt nem tudom, hogy hova akarjuk majd írni és honna akarjuk meghívni (akkor kell meghívni amikor a játékos lépni akar)
+    //TODO::kellene egy játék kiíró de az sztem ugyanaz lenne mint a file-os
+    /**
+     * Parancsok olvasása a játékhoz
+     */
+    private Scanner console_read = new Scanner(System.in);
+    public void readCommands(){
+        boolean correct = false;
+        String in;
+        String[] commands;
+        Settler se;
+
+        while(!correct){
+            in = console_read.nextLine();
+            commands = in.split("[ !\"\\#$%&'*+,-./:;<=>?@\\[\\]^_`{|}~]+");
+
+            switch (commands[0]){
+                 case "move":{
+                     if(commands.length != 3 || Integer.parseInt(commands[1]) >= field.getSettlers().size() || Integer.parseInt(commands[2]) >= field.getAsteroids().size()){
+                         System.out.println("Helytelen parancs!");
+                         correct = false;
+                         break;
+                     }
+                     se =  field.getSettlers().get(Integer.parseInt(commands[1]));
+                     se.Move(field.getAsteroids().get(Integer.parseInt(commands[2])));
+                     correct = true;
+                }
+                case "drill":{
+                    if(commands.length != 2 || Integer.parseInt(commands[1]) > field.getSettlers().size()){
+                        System.out.println("Helytelen parancs!");
+                        correct = false;
+                        break;
+                    }
+                    se =  field.getSettlers().get(Integer.parseInt(commands[1]));
+                    se.Drill();
+                    correct = true;
+                }
+                case "mine":{
+                    if(commands.length != 2 || Integer.parseInt(commands[1]) > field.getSettlers().size()){
+                        System.out.println("Helytelen parancs!");
+                        correct = false;
+                        break;
+                    }
+                    se =  field.getSettlers().get(Integer.parseInt(commands[1]));
+                    se.Mine();
+                    correct = true;
+                }
+                case "useteleport":{
+                    if(commands.length != 3 || Integer.parseInt(commands[1]) >= field.getSettlers().size() || Integer.parseInt(commands[2]) >= field.getSettlers().get(Integer.parseInt(commands[1])).getAsteroid().getTeleports().size()){
+                        System.out.println("Helytelen parancs!");
+                        correct = false;
+                        break;
+                    }
+                    se =  field.getSettlers().get(Integer.parseInt(commands[1]));
+                    se.UseTeleport(se.getAsteroid().getTeleports().get(Integer.parseInt(commands[2])));
+                    correct = true;
+                }
+                case "placeteleport":{
+                    if(commands.length != 2 || Integer.parseInt(commands[1]) > field.getSettlers().size()){
+                        System.out.println("Helytelen parancs!");
+                        correct = false;
+                        break;
+                    }
+                    se =  field.getSettlers().get(Integer.parseInt(commands[1]));
+                    se.PlaceTeleport();
+                    correct = true;
+                }
+                case "placematerial":{
+                    if(commands.length != 2 || Integer.parseInt(commands[1]) > field.getSettlers().size()){
+                        System.out.println("Helytelen parancs!");
+                        correct = false;
+                        break;
+                    }
+                    se =  field.getSettlers().get(Integer.parseInt(commands[1]));
+                    se.PlaceMaterial();
+                    correct = true;
+                }
+                case "maketeleport":{
+                    if(commands.length != 2 || Integer.parseInt(commands[1]) > field.getSettlers().size()){
+                        System.out.println("Helytelen parancs!");
+                        correct = false;
+                        break;
+                    }
+                    se =  field.getSettlers().get(Integer.parseInt(commands[1]));
+                    se.MakeTeleport();
+                    correct = true;
+                }
+                case "buildrobot":{
+                    if(commands.length != 2 || Integer.parseInt(commands[1]) > field.getSettlers().size()){
+                        System.out.println("Helytelen parancs!");
+                        correct = false;
+                        break;
+                    }
+                    se =  field.getSettlers().get(Integer.parseInt(commands[1]));
+                    se.BuildRobot();
+                    correct = true;
+                }
+                case "savegame":{  //TODO::menuben lehet majd beolvasni (loadgame-et ott kell meghívni)
+                    if(commands.length != 1){
+                        System.out.println("Helytelen parancs!");
+                        correct = false;
+                        break;
+                    }
+                    try {
+                        saveGame();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    correct = true;
+                }
+                default:{
+                    System.out.println("Helytelen parancs!");
+                    correct = false;
+                    break;
+                }
+            }
+        }
+    }
 }
