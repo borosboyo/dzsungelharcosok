16057f71b36b24bc074d2da997f3c8d3e655a10e
Bálint Biros <bbalint2000@gmail.com>
2021-04-23 00:42:30 +0200

    coords

--- src/model/Asteroid.java
+++ src/model/Asteroid.java
@@ -12,6 +12,7 @@ public class Asteroid implements Serializable {
 
 
     private int id;
+
     private int x, y;
     private  boolean nearSun;
     private  Material material;
@@ -360,5 +361,23 @@ public class Asteroid implements Serializable {
     public void setId(int id) {
         this.id = id;
     }
+
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
 
 }
