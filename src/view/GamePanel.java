a09442ab3eeaa938564e84140e9aebc9a76bf3fd
IBarnabas <barnabas.ivancsics@gmail.com>
2021-04-22 23:26:23 +0200

    Draw

--- src/view/GamePanel.java
+++ src/view/GamePanel.java
@@ -2,7 +2,8 @@ package view;
 
 import model.Field;
 import model.Game;
-import model.Material;
+import model.Entity;
+import java.io.*;
 
 import javax.swing.*;
 import java.awt.*;
@@ -17,15 +18,20 @@ public class GamePanel extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-
         Graphics2D window_frame = (Graphics2D) g;
-
         Field fi = Game.getInstance().field;
-        MaterialView mat = null;
+        MaterialView mat;
 
-        //Asteroids draw
+        this.setBackground(Color.BLACK);
+
+        /**
+         * Asteroids draw
+         */
         fi.getAsteroids().stream().map(asteroid -> new AsteroidView(asteroid)).forEach(asteroidView -> asteroidView.draw(window_frame));
 
+        /**
+         *  Teleport, Entity, Material draw
+         */
         for (int i = 0; i < fi.getAsteroids().size(); i++){
             fi.getAsteroids().get(i).getEntities().stream().map(entity -> new EntityView(entity)).forEach(entityView -> entityView.draw(window_frame));
             fi.getAsteroids().get(i).getTeleports().stream().map(teleport -> new TeleportView(teleport)).forEach(teleportView -> teleportView.draw(window_frame));
@@ -33,5 +39,15 @@ public class GamePanel extends JPanel {
             mat.draw(window_frame);
         }
 
+
+        //TODO::itt lehetne fontot is allitani es a szovegek helyet is megkene , mas szovegeket is leheten itt megadni, ha kell
+        //Font font = new Font(Font.SERIF, Font.PLAIN, (int) (0.6 * unit));
+        g.setColor(Color.WHITE);
+        g.drawString("Settler turn:",0,0);
+        for(int i = 0; i < fi.getSettlers().size(); i++){
+            if(!fi.getSettlers().get(i).isFinishedTurn()){
+                g.drawString(String.valueOf(fi.getSettlers().get(i).getId()),0,2+i);
+            }
+        }
     }
 }
