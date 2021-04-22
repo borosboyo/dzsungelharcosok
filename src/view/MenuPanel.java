74cd04446bd64d412948e976266e84cd8380eb3a
borospc <borosgergo@yahoo.com>
2021-04-22 22:32:34 +0200

    Minor changes

--- src/view/MenuPanel.java
+++ src/view/MenuPanel.java
@@ -1,6 +1,12 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MenuPanel extends JPanel {
+    public MenuPanel(){
+        setLayout(new GridLayout(2,2,20,20));
+        setBorder(BorderFactory.createEmptyBorder(50,75,50,75));
+        setBackground(Color.lightGray);
+    }
 }
