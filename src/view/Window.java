78e73a506bdb0fd8dae98e2fed46401ebafd38ff
borospc <borosgergo@yahoo.com>
2021-04-22 22:19:00 +0200

    Main osztaly

--- src/view/Window.java
+++ src/view/Window.java
@@ -1,8 +1,17 @@
 package view;
 
 import javax.swing.*;
+import java.io.IOException;
 
 public class Window extends JFrame {
-    Menu menu;
-    Game game;
+    private Menu menu;
+    private Game game;
+
+    public Window(){
+        menu = new Menu();
+    }
+
+    public static void main(String []args) throws IOException {
+        new Window();
+    }
 }
