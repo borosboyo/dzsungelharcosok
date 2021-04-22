11a684b7fff1ebe63db93d28b4c95b58ab61583a
borospc <borosgergo@yahoo.com>
2021-04-22 22:21:01 +0200

    Main osztaly

--- src/view/Window.java
+++ src/view/Window.java
@@ -1,14 +1,23 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
 
 public class Window extends JFrame {
-    private Menu menu;
+    private Menu menu = new Menu();
     private Game game;
 
     public Window(){
-        menu = new Menu();
+        setTitle("Minesweeper");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(800, 800);
+        setResizable(false);
+        menu.setLayout(new GridLayout(2,2,20,20));
+        menu.setBorder(BorderFactory.createEmptyBorder(50,75,50,75));
+        menu.setBackground(Color.lightGray);
+        add(menu);
+        setVisible(true);
     }
 
     public static void main(String []args) throws IOException {
