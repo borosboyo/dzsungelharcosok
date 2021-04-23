f6687b9d135fc57637379c2ede3a9a47ccf834fa
borospc <borosgergo@yahoo.com>
2021-04-24 00:30:12 +0200

    Gamepanel listener not working

--- src/view/GamePanel.java
+++ src/view/GamePanel.java
@@ -22,49 +22,13 @@ public class GamePanel extends JPanel {
         this.window = _window;
         this.unit = unit;
         //TODO::ez így nem működik
-        addKeyListener(new KeyListener() {
-            @Override
-            public void keyTyped(KeyEvent e) {            }
+        setFocusable(true);
+        addKeyListener(new KeyListenerClass());
+    }
 
-            @Override
-            public void keyPressed(KeyEvent e) {
-                if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
-                    //Game.getInstance().getMenu().setMenuState(MenuState.LOADMENU);
-                    //Game.getInstance().getMenu().menu_step(0);
-                    //TODO::Boros swith to MenuPanel
-                    System.exit(0); //Ezt utána ki lehet szedni
-                }
-                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-                    for (Asteroid a : Game.getInstance().field.getAsteroids()) {
-                        int newY = a.getY() - 10;
-                        a.setY(newY);
-                    }
-                }
-                if (e.getKeyCode() == KeyEvent.VK_UP){
-                    for(Asteroid a : Game.getInstance().field.getAsteroids()){
-                        int newY = a.getY() + 10;
-                        a.setY(newY);
-                    }
-                }
-                if (e.getKeyCode() == KeyEvent.VK_RIGHT){
-                    for(Asteroid a : Game.getInstance().field.getAsteroids()){
-                        int newX = a.getX() +10;
-                        a.setX(newX);
-                    }
-                }
-                if (e.getKeyCode() == KeyEvent.VK_LEFT){
-                    for(Asteroid a : Game.getInstance().field.getAsteroids()){
-                        int newX = a.getX() -10;
-                        a.setX(newX);
-                    }
-                }
-
-            }
-
-            @Override
-            public void keyReleased(KeyEvent e) {
-            }
-        });
+    @Override
+    public boolean isFocusTraversable() {
+        return true;
     }
 
     @Override
@@ -83,11 +47,58 @@ public class GamePanel extends JPanel {
         Font font = new Font(Font.SERIF, Font.PLAIN, (int) (20));
         g.setFont(font);
         g.setColor(Color.WHITE);
-        g.drawString("Settler turn:",2,20);
-        for(int i = 0; i < fi.getSettlers().size(); i++){
-            if(!fi.getSettlers().get(i).isFinishedTurn()){
-                g.drawString(String.valueOf(fi.getSettlers().get(i).getId()),0,40+ (i*20));
+        g.drawString("Settler turn:", 2, 20);
+        for (int i = 0; i < fi.getSettlers().size(); i++) {
+            if (!fi.getSettlers().get(i).isFinishedTurn()) {
+                g.drawString(String.valueOf(fi.getSettlers().get(i).getId()), 0, 40 + (i * 20));
             }
+        }
+    }
+
+
+    class KeyListenerClass implements KeyListener {
+
+        @Override
+        public void keyTyped(KeyEvent e) {
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+                //Game.getInstance().getMenu().setMenuState(MenuState.LOADMENU);
+                //Game.getInstance().getMenu().menu_step(0);
+                //TODO::Boros swith to MenuPanel
+                System.exit(0); //Ezt utána ki lehet szedni
+            }
+            if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                for (Asteroid a : Game.getInstance().field.getAsteroids()) {
+                    int newY = a.getY() - 10;
+                    a.setY(newY);
+                }
+            }
+            if (e.getKeyCode() == KeyEvent.VK_UP) {
+                for (Asteroid a : Game.getInstance().field.getAsteroids()) {
+                    int newY = a.getY() + 10;
+                    a.setY(newY);
+                }
+            }
+            if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                for (Asteroid a : Game.getInstance().field.getAsteroids()) {
+                    int newX = a.getX() + 10;
+                    a.setX(newX);
+                }
+            }
+            if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                for (Asteroid a : Game.getInstance().field.getAsteroids()) {
+                    int newX = a.getX() - 10;
+                    a.setX(newX);
+                }
+            }
+
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
         }
     }
 
