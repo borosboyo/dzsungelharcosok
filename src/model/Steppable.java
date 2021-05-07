a6ea0f5ad710501aeb16ae02e7791aca2783b783
IBarnabas <barnabas.ivancsics@gmail.com>
2021-05-08 01:13:53 +0200

    Steppable code clean

--- src/model/Steppable.java
+++ src/model/Steppable.java
@@ -4,13 +4,8 @@ package model;
  * Steppable interface that the classes Robot and Field implements.
  */
 public interface Steppable {
-
-    int counter=0;
-
     /**
      * The Step function that determines what action should a Robot or the Field do in a step.
      */
-    public void Step();
-
-
+    void Step();
 }
