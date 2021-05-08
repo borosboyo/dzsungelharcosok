f8dc2341623eb429ede4f4713267cb77650ea0cc
borospc <borosgergo@yahoo.com>
2021-05-08 23:55:17 +0200

    Menu comments

--- src/model/Menu.java
+++ src/model/Menu.java
@@ -2,21 +2,36 @@ package model;
 
 import java.io.IOException;
 
+/**
+ * The type Menu.
+ */
 public class Menu {
     private MenuState menuState = MenuState.LOADMENU;
 
+    /**
+     * Sets menu state.
+     *
+     * @param menuState the menu state
+     */
     public void setMenuState(MenuState menuState) {
         this.menuState = menuState;
     }
 
-    public void menu_step(int settler_numb){
-        switch (menuState){
-            case NEWGAME:{
+    /**
+     * Handles the different scenarios based on menu states.
+     *
+     * @param settler_numb the settler numb
+     */
+    public void menu_step(int settler_numb) {
+        switch (menuState) {
+            //New game starts
+            case NEWGAME: {
                 Game.getInstance().resetGame(settler_numb);
                 Game.getInstance().setGameState(GameState.GAME);
                 Game.getInstance().getMenu().setMenuState(MenuState.NEWGAME);
                 break;
             }
+            //The user loads a game.
             case LOADGAME:{
                 Game.getInstance().field = null;
                 try {
@@ -28,6 +43,7 @@ public class Menu {
                 Game.getInstance().getMenu().setMenuState(MenuState.LOADGAME);
                 break;
             }
+            //Loadmenu state
             case LOADMENU:{
                 Game.getInstance().setGameState(GameState.MENU);
                 Game.getInstance().getMenu().setMenuState(MenuState.LOADMENU);
